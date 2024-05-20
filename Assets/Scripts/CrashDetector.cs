4cc71822846d855ef11f7538cfa5534f1f46cce0
ozerres <eskimezozer@gmail.com>
2024-05-20 19:07:56 +0300

    Particles
    Added particle effects on finish and crash

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float delayTime = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            crashEffect.Play();
+            Invoke("ReloadScene", delayTime);
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
