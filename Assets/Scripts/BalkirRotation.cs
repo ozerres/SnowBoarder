36a7815b84186077f7e6594b8e7bef6c1fdcb768
ozerres <eskimezozer@gmail.com>
2024-05-21 13:16:48 +0300

    Game completed.

--- Assets/Scripts/BalkirRotation.cs
+++ Assets/Scripts/BalkirRotation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +7,50 @@ public class BalkirRotation : MonoBehaviour
 {
 
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+    bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if(canMove == true)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
+        
+    }
+
+    public void DisableControls()
+    {
+        canMove = false;
+    }
+
+    void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(torqueAmount);
         }
--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -8,10 +8,14 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float delayTime = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
 
+    bool hasCrashed = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Ground")
+        if (other.tag == "Ground" && !hasCrashed)
         {
+            hasCrashed = true;
+            FindObjectOfType<BalkirRotation>().DisableControls();
             crashEffect.Play();
             Invoke("ReloadScene", delayTime);
         }
